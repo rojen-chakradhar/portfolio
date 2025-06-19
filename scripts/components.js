a5c666a402604589699ce8b5b59446c4a93a1adf
Rojen Chakradhar <chakradhar@rojen.me>
2025-06-19 15:00:13 +0545

    add flowers component and integrate into main page; update styles for text and animations

--- scripts/components.js
+++ scripts/components.js
@@ -6,6 +6,18 @@ function loadComponent(id, file) {
     });
 }
 
+function injectFlowersToAll() {
+  fetch('/components/flowers.html')
+    .then(res => res.text())
+    .then(html => {
+      document.querySelectorAll('.flowers').forEach(el => {
+        el.innerHTML = html;
+      });
+    });
+}
+
+injectFlowersToAll();
+
 loadComponent("navbar", "/components/navbar.html");
 loadComponent("sidenav", "/components/sidenav.html");
 loadComponent("footer", "/components/footer.html");
