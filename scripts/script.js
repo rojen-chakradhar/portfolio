bc72513efadc488e40dd4b5b466bde5adda4325d
Rojen Chakradhar <chakradhar@rojen.me>
2025-10-17 22:19:58 +0545

    idk what i did and i think my friend ruined something. so just wanna commit.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,29 @@
 let tab = document.querySelectorAll(".tab");
-let projects = document.querySelectorAll(".project-card");
+let projects = document.querySelectorAll(".project-box");
+let grid = document.querySelectorAll(".grid");
 
 tab.forEach((elm) => {
 	elm.addEventListener("click", () => {
 		tab.forEach((btn) => btn.classList.remove("active"));
 		elm.classList.add("active");
 		let category = elm.textContent.toLowerCase();
+		let visibleCount = 0;
 		projects.forEach((proj) => {
 			if (category === "all") {
 				proj.style.display = "block";
+				visibleCount++;
 			} else {
 				if (proj.classList.contains(category)) {
 					proj.style.display = "block";
+					visibleCount++;
 				} else {
 					proj.style.display = "none";
 				}
 			}
+		});
+		const rows = Math.ceil(visibleCount / 3);
+		grid.forEach((g) => {
+			g.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 		});
 	});
 });
