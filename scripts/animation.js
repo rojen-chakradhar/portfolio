cb92c2669302da5a60c6963950b138aaa124e342
Rojen Chakradhar <chakradhar@rojen.me>
2026-02-28 00:44:01 +0545

    loader, navbar and gsap and smooth scrolling

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,5 +1,12 @@
-let nav = document.querySelector(".nav-links");
-setTimeout(() => {
-	nav.style.padding = "0.2rem 1rem";
-	nav.style.width = "100%";
-}, 1000);
+var tl = gsap.timeline();
+tl.to("#loader", 3, {
+	height: "0",
+	display: "none",
+	ease: Power4.easeInOut,
+	delays: "0.5",
+});
+
+tl.from(".navbar", 1, {
+	y: -100,
+	ease: Power4.easeOut,
+})
