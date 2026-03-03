49ca9221176bccba1912c3bf840d89ba645d3c65
Rojen Chakradhar <chakradhar@rojen.me>
2026-03-03 23:46:20 +0545

    new design

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,12 +1,32 @@
 var tl = gsap.timeline();
-tl.to("#loader", 3, {
-	height: "0",
+tl.to(".loader1", 3, {
+	// height: "0",
+	top: "-100dvh",
 	display: "none",
 	ease: Power4.easeInOut,
 	delays: "0.5",
 });
-
 tl.from(".navbar", 1, {
 	y: -100,
+	ease: Power4.easeOut,
+})
+
+tl.from(".swiper-wrapper", 1, {
+	x: 1300,
+	ease: Power4.easeOut,
+})
+
+tl.from(".swiper-button-prev", 1, {
+	x: -500,
+	ease: Power4.easeOut,
+})
+
+tl.from(".swiper-button-next", 1, {
+	x: 500,
+	ease: Power4.easeOut,
+})
+
+tl.from(".swiper-pagination", 1, {
+	y: 200,
 	ease: Power4.easeOut,
 })
--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,12 @@
-let tab = document.querySelectorAll(".tab");
-let projects = document.querySelectorAll(".project-box");
-let grid = document.querySelectorAll(".grid");
-
-tab.forEach((elm) => {
-	elm.addEventListener("click", () => {
-		tab.forEach((btn) => btn.classList.remove("active"));
-		elm.classList.add("active");
-		let category = elm.textContent.toLowerCase();
-		let visibleCount = 0;
-		projects.forEach((proj) => {
-			if (category === "all") {
-				proj.style.display = "block";
-				visibleCount++;
-			} else {
-				if (proj.classList.contains(category)) {
-					proj.style.display = "block";
-					visibleCount++;
-				} else {
-					proj.style.display = "none";
-				}
-			}
-		});
-		const rows = Math.ceil(visibleCount / 3);
-		grid.forEach((g) => {
-			g.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-		});
-	});
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+	draggable: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
-
-// let counter = document.getElementById("counter");
-// let main = document.querySelector("main");
-// let loader = document.querySelector(".loader");
-// let count = 0;
-// let interval = setInterval(() => {
-// 	main.style.display = "none";
-// 	if (count < 100) {
-// 		count++;
-// 		counter.style.opacity = count / 100;
-// 		counter.innerText = count + "%";
-// 	} else {
-// 		clearInterval(interval);
-// 		setTimeout(() => {
-// 			loader.style.display = "none";
-// 			main.style.display = "block";
-// 		}, 700);
-// 	}
-// }, 40);
