59f5fa23de25079c521ddb629cb6ea6039dd838e
Rojen Chakradhar <chakradhar@rojen.me>
2026-03-05 00:42:10 +0545

    testing different color palletes and trying to make gallery work

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,6 +1,6 @@
 var tl = gsap.timeline();
 tl.to("#loader", 3, {
-	height: "0",
+	y: -700,
 	display: "none",
 	ease: Power4.easeInOut,
 	delays: "0.5",
@@ -9,46 +9,68 @@ tl.to("#loader", 3, {
 tl.from(".logo", 1, {
 	y: -100,
 	ease: Power4.easeOut,
-})
+});
 
 tl.from(".nav-links", 1, {
 	y: -100,
 	ease: Power4.easeOut,
-})
+});
 
 tl.from(".loc", 1, {
 	x: -500,
 	ease: Power4.easeOut,
-})
+});
 
 tl.from(".me", 1, {
 	x: 500,
 	ease: Power4.easeOut,
-})
+});
 
 tl.from(".name", 1, {
 	marginLeft: 1300,
 	ease: Power4.easeOut,
-})
+});
+
+const triggers = ScrollTrigger.getAll();
+triggers.forEach((trigger) => trigger.kill());
 
 const horTextAnimation = () => {
-  const triggers = ScrollTrigger.getAll();
-  triggers.forEach((trigger) => trigger.kill());
-  const horText = document.querySelector(".name");
-  if (horText) {
-    gsap.to(horText, {
-      x: () => -(horText.offsetWidth - window.innerWidth),
-      ease: "linear",
-      scrollTrigger: {
-        trigger: ".hero",
-        pin: true,
-        scrub: true,
-        invalidateOnRefresh: true,
-        end: () => `+=${horText.offsetWidth - window.innerWidth}`
-      }
-    });
-    ScrollTrigger.refresh();
-  }
+	const horText = document.querySelector(".name");
+	if (horText) {
+		gsap.to(horText, {
+			x: () => -(horText.offsetWidth - window.innerWidth),
+			ease: "linear",
+			scrollTrigger: {
+				trigger: ".hero",
+				pin: true,
+				scrub: true,
+				invalidateOnRefresh: true,
+				end: () => `+=${horText.offsetWidth - window.innerWidth}`,
+			},
+		});
+		ScrollTrigger.refresh();
+	}
 };
 horTextAnimation();
 window.addEventListener("resize", horTextAnimation);
+
+// const galleryScroll = () => {
+// 	const works = document.querySelector(".works");
+// 	if (works) {
+// 		gsap.to(works, {
+// 			x: () => -(works.offsetWidth - window.innerWidth),
+// 			ease: "linear",
+// 			scrollTrigger: {
+// 				trigger: ".projects",
+// 				pin: true,
+// 				scrub: true,
+// 				invalidateOnRefresh: true,
+// 				end: () => `+=${works.offsetWidth - window.innerWidth}`,
+// 				markers: true,
+// 			},
+// 		});
+// 		ScrollTrigger.refresh();
+// 	}
+// };
+// galleryScroll();
+// window.addEventListener("resize", galleryScroll);
