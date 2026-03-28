215c06de135522e7ae577b3f59fb46bb21283b63
Rojen Chakradhar <chakradhar@rojen.me>
2026-03-29 02:53:05 +0545

    sidenav created

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,8 +1,8 @@
 gsap.to("#loader", 3, {
-	y: -700,
+	y: -800,
 	display: "none",
 	ease: Power4.easeInOut,
-	delays: "0.2",
+	delays: 0.2,
 });
 
 const triggers = ScrollTrigger.getAll();
@@ -27,3 +27,33 @@ const horTextAnimation = () => {
 };
 horTextAnimation();
 window.addEventListener("resize", horTextAnimation);
+
+const tl = gsap.timeline({paused: true, defaults: {ease: "expo.inOut"}});
+
+tl.to(".sidenav", 1.2, {
+	autoAlpha: 1,
+	clipPath: "circle(150% at 100% 0%)",
+	startAt: {clipPath: "circle(0% at 100% 0%)"}
+});
+
+tl.from(".side-nav-text", 1, {
+	y: 150,
+	stagger: 0.1,
+	ease: "power4.out"
+}, "-=0.6");
+
+function menuOpen() {
+	tl.play();
+}
+
+function menuClose() {
+	tl.reverse();
+}
+
+const sideNavLinks = document.querySelectorAll(".side-nav-text");
+
+sideNavLinks.forEach(link => {
+	link.addEventListener("click", () => {
+		tl.reverse();
+	})
+})
