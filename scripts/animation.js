5f5f575c8fc7e28b8fd165db56751dc52871a2b7
Rojen Chakradhar <chakradhar@rojen.me>
2026-03-29 17:05:18 +0545

    fixing

---
+++ scripts/animation.js
@@ -0,0 +1,65 @@
+const scroller = new LocomotiveScroll();
+
+const revealSite = () => {
+	const loader = document.querySelector("#loader");
+	if (loader) {
+		gsap.to(loader, {
+			y: "-100%",
+			duration: 1.5,
+			ease: "expo.inOut",
+			delay: 0.2,
+			onComplete: () => {
+				loader.style.display = "none";
+				horTextAnimation();
+			},
+		});
+	}
+};
+
+const horTextAnimation = () => {
+	const horText = document.querySelector(".name");
+	if (horText) {
+		gsap.to(horText, {
+			x: () => -(horText.offsetWidth - window.innerWidth),
+			ease: "none",
+			scrollTrigger: {
+				trigger: ".hero",
+				pin: true,
+				scrub: 1,
+				invalidateOnRefresh: true,
+				start: "top top",
+				end: () => `+=${horText.offsetWidth}`,
+			},
+		});
+	}
+};
+
+const forceTimer = setTimeout(revealSite, 3000);
+
+window.addEventListener("load", () => {
+	clearTimeout(forceTimer);
+	revealSite();
+});
+
+const tl = gsap.timeline({ paused: true, defaults: { ease: "expo.inOut" } });
+
+tl.to(".sidenav", 1.2, {
+	autoAlpha: 1,
+	clipPath: "circle(150% at 100% 0%)",
+	startAt: {
+		clipPath: "circle(0% at 100% 0%)"
+	}
+});
+
+tl.from(".side-nav-text", 1, {
+		y: 150,
+		stagger: 0.1,
+		ease: "power4.out"
+	}, "-=0.6",);
+
+function menuOpen() {tl.play();}
+function menuClose() {tl.reverse();}
+
+document.querySelectorAll(".side-nav-text").forEach((link) => {
+	link.addEventListener("click", () => {tl.reverse();});
+});
